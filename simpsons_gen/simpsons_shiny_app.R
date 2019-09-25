82dea90d9856e74a5f2772f736362fe298784888
markloomis32 <markloomis32@gmail.com>
2019-09-25 17:19:22 -0400

    removed dplyr dependency; cleaned csv
    removed dplyr dependency; cleaned csv outside of script

--- simpsons_gen/simpsons_shiny_app.R
+++ simpsons_gen/simpsons_shiny_app.R
@@ -1,22 +1,12 @@
 #install packages
 if (!require(shiny)) install.packages('shiny')
-if (!require(dplyr)) install.packages('dplyr')
 library(shiny)
-library(dplyr)
 
 ## Load Simpsons Data
-simpsons_script <- read.csv("simpsons_script.csv",  sep=",")
+simpsons_script <- read.csv("simpsons_script_clean.csv",  sep=",")
 
-## add row number to simpsons_script
-simpsons_script$rn <- seq.int(nrow(simpsons_script))
-simpsons_script3 <- subset(simpsons_script,  speaking_line == "true" & spoken_words != "/" & word_count != "0" & word_count != "1" & word_count != "2" & word_count != "3" & word_count != "4"& word_count != "5")
-
-##narrow character list to 500 or more quotes
-character_options <- simpsons_script %>% 
-    select(raw_character_text) %>%
-    count(raw_character_text, sort = TRUE, name = "quote_count")   %>% 
-    group_by(raw_character_text)  %>% 
-    filter(quote_count > 100 , raw_character_text != "")
+## Character Options
+character_options <- unique(simpsons_script$raw_character_text) 
 
 #quotegen function
 
@@ -24,7 +14,6 @@ quote_gen <- function(simpsons_char,env = parent.frame()) {
     filtered_char <<- filter(simpsons_script, raw_character_text == as.character(simpsons_char))
     quoteoftheday <<- sample_n(filtered_char, size = 1)
     quote_display <<- quoteoftheday$spoken_words
-   
 }
 
 ui <- fluidPage(
@@ -36,7 +25,7 @@ ui <- fluidPage(
             ##inputfunction
             selectInput(inputId = "simp_char", 
                         label = "Choose a Simpsons Character", 
-                        choices = character_options$raw_character_text
+                        choices = character_options
             )
         ),
         
@@ -69,5 +58,7 @@ server <- function(input, output) {
     
 }
 shinyApp(ui = ui, server = server)
+
+
 
 
